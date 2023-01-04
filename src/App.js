23bb70e729f207bbbd230886924bbe88139d70c1
Raja-Paul <101011683+Raja-Paul@users.noreply.github.com>
2023-01-05 00:08:22 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Hero from './Hero'
+import Navbar from './Navbar'
+import Client from './Client'
+import About from './About'
+import Service_cta from './Service_cta'
+import Faq from './Faq'
+import Contact from './Contact'
+import Footer from './Footer'
+import Registration_form from './Registration_form'
+import './index.css'
+// import {
+//   BrowserRouter as Routes,
+//   Router,
+//   Route,
+//   Link
+// } from "react-router-dom";
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  return (<>
+
+  {/* <Router> */}
+    <Navbar/>
+   <Registration_form/>
+    <Hero/>
+    {/* <Client/>  */}
+    {/* <About /> */}
+     {/* <Routes>
+          <Route exact path="/">
+          <About/>
+          </Route>
+        </Routes> */}
+    {/* <About/> */}
+    <Faq/>
+    <Service_cta/>
+    {/* <Contact/> */}
+    <Footer/>
+    {/* </Router> */}
+    </>
   );
 }
 
